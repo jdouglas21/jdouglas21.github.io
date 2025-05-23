6a68dda0335040be3fe56a0e8e5bc77b50fc6c26
jhdouglas21 <jhdouglasjr21@gmail.com>
2025-05-22 21:36:01 -0400

    Animation Attempt 2

---
+++ js/app.js
@@ -0,0 +1,44 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  },
+  retina_detect: true
+});
